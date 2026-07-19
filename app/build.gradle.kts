ee34198fa61694e68c994aa3683b8dfb9ace48ea
NathanLenias <nathan.balencie@gmail.com>
2026-07-19 16:03:59 +0200

    chore: bump to 1.2.2 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.nathanb.lock"
         minSdk = 33
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.2.1"
+        versionCode = 10
+        versionName = "1.2.2"
     }
 
     signingConfigs {
